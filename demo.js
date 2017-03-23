6b690b39bb2789f57331eecbded4b549114ae0a0
Eric LEON <ericp.leon@gmail.com>
2017-03-23 12:13:34 +0100

    Shopping List with component

--- demo.js
+++ demo.js
@@ -3,58 +3,64 @@
 /**
  * The AngularJS shopping list app
  */
-const shoppingListApp = angular.module('shoppingApp', [
-  'shoppingList'
-]);
+const shoppingListApp = angular.module('shoppingApp', []);
 
 /**
- * The shoppingList module
+ * The shoppingList component
  */
-const shoppingList = angular.module('shoppingList', []);
+shoppingListApp.component("shoppingList", {
 
-/**
- * The sohppingList controller
- */
-shoppingList.controller('shoppingCtrl', ['$scope', function ($scope) {
+    templateUrl: '/view.html',
 
-  // Initialize our model
-  $scope.model = {
-    list: [],
-    newItem: '',
-    allChecked: false
-  };
+    bindings: {
+      list: '<',
+      newItem: '@',
+      allChecked: '<'
+    },
 
-  // Add a new item
-  $scope.addItem = function () {
-    var newItem = $scope.model.newItem.trim();
-    if (newItem.length) {
-      // Add new item to the items list
-      $scope.model.list.push({
-        title: newItem,
-        completed: false
-      });
-      // Stay ready for new item input
-      $scope.model.newItem = '';
+    controller: function () {
+
+      var vm = this;
+
+      vm.$onInit = () => {
+        vm.list = [];
+        vm.newItem = '';
+        vm.allChecked = false;
+      };
+
+      // Add a new item
+      vm.addItem = () => {
+        var item = vm.newItem.trim();
+        if (item.length) {
+          // Add new item to the items list
+          vm.list.push({
+            title: vm.newItem,
+            completed: false
+          });
+          // Stay ready for new item input
+          vm.newItem = '';
+        }
+      };
+
+      // Remove an existing item
+      vm.removeItem = (item) => {
+        vm.list.splice(vm.list.indexOf(item), 1);
+      };
+
+      // Toggle items completion flag
+      vm.markAll = (completed) => {
+        vm.list.forEach(function (item) {
+          item.completed = !completed;
+        });
+        vm.allChecked = !completed;
+      };
+
+      // Remove all checked items
+      vm.clearCompletedItems = () => {
+        vm.list = vm.list.filter(function (item) {
+          return !item.completed;
+        });
+      };
+
     }
-  };
-
-  // Remove an existing item
-  $scope.removeItem = function (item) {
-    $scope.model.list.splice($scope.model.list.indexOf(item), 1);
-  };
-
-  // Toggle items completion flag
-  $scope.markAll = function (completed) {
-    $scope.model.list.forEach(function (item) {
-      item.completed = !completed;
-    });
-    $scope.model.allChecked = !completed;
-  };
-
-  // Remove all checked items
-  $scope.clearCompletedItems = function () {
-    $scope.model.list = $scope.model.list.filter(function (item) {
-      return !item.completed;
-    });
-  };
-}]);
+});
