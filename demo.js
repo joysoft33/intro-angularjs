b70b8862fe5b79cad52172f3f7dfa1df4d130bad
Eric LEON <ericp.leon@gmail.com>
2017-03-29 15:52:44 +0200

    Simplified version

--- demo.js
+++ demo.js
@@ -3,58 +3,47 @@
 /**
  * The AngularJS shopping list app
  */
-const shoppingListApp = angular.module('shoppingApp', [
-  'shoppingList'
-]);
+angular.module('shoppingApp', [])
 
-/**
- * The shoppingList module
- */
-const shoppingList = angular.module('shoppingList', []);
+  .controller('shoppingCtrl', function ($scope) {
 
-/**
- * The sohppingList controller
- */
-shoppingList.controller('shoppingCtrl', ['$scope', function ($scope) {
+    // Initialize our model
+    $scope.model = {
+      list: [],
+      newItem: '',
+      allChecked: false
+    };
+    
+    // Add a new item
+    $scope.addItem = function () {
+      var newItem = $scope.model.newItem.trim();
+      if (newItem.length) {
+        // Add new item to the items list
+        $scope.model.list.push({
+          title: newItem,
+          completed: false
+        });
+        // Stay ready for new item input
+        $scope.model.newItem = '';
+      }
+    };
 
-  // Initialize our model
-  $scope.model = {
-    list: [],
-    newItem: '',
-    allChecked: false
-  };
+    // Remove an existing item
+    $scope.removeItem = function (item) {
+      $scope.model.list.splice($scope.model.list.indexOf(item), 1);
+    };
 
-  // Add a new item
-  $scope.addItem = function () {
-    var newItem = $scope.model.newItem.trim();
-    if (newItem.length) {
-      // Add new item to the items list
-      $scope.model.list.push({
-        title: newItem,
-        completed: false
+    // Toggle items completion flag
+    $scope.markAll = function (completed) {
+      completed = !completed;
+      $scope.model.list.forEach(function (item) {
+        item.completed = completed;
       });
-      // Stay ready for new item input
-      $scope.model.newItem = '';
-    }
-  };
+      $scope.model.allChecked = completed;
+    };
 
-  // Remove an existing item
-  $scope.removeItem = function (item) {
-    $scope.model.list.splice($scope.model.list.indexOf(item), 1);
-  };
-
-  // Toggle items completion flag
-  $scope.markAll = function (completed) {
-    $scope.model.list.forEach(function (item) {
-      item.completed = !completed;
-    });
-    $scope.model.allChecked = !completed;
-  };
-
-  // Remove all checked items
-  $scope.clearCompletedItems = function () {
-    $scope.model.list = $scope.model.list.filter(function (item) {
-      return !item.completed;
-    });
-  };
-}]);
+    // Remove all checked items
+    $scope.clearCompletedItems = function () {
+      $scope.model.list = $scope.model.list.filter((item) => !item.completed);
+    };
+  });
